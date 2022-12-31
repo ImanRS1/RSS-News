4cf1f9277c0689e1054eb35f17a9492ecd619df1
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-12-31 21:35:14 +0100

    styled-components installed

--- pages/api/fetchData.ts
+++ pages/api/fetchData.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -9,7 +8,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log("hej, ", req.body);
-
   res.status(200).send({ name: "hej från backend" });
 }
