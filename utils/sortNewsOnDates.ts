ab68c29bce4558ee887176dd7b2b3f5ad13bb8c3
Iman Reza Soltani <iman.soltani@bonniernews.se>
2023-05-08 15:44:24 +0200

    refactoring

--- utils/sortNewsOnDates.ts
+++ utils/sortNewsOnDates.ts
@@ -5,12 +5,12 @@ export default function sortNewsOnDates(
   unsortedArray: rssData[],
   totalFetch?: boolean
 ): rssData[] {
-  const newAr: rssData[] = unsortedArray.sort(
+  const dateSortedArray: rssData[] = unsortedArray.sort(
     (a: rssData, b: rssData) =>
       new Date(b.date).valueOf() - new Date(a.date).valueOf()
   );
 
-  if (totalFetch) return reduceAmountOfSportNews(newAr).slice(0, 20);
+  if (totalFetch) return reduceAmountOfSportNews(dateSortedArray).slice(0, 20);
 
-  return newAr.slice(0, 20);
+  return dateSortedArray.slice(0, 20);
 }
--- utils/urlArray.ts
+++ utils/urlArray.ts
@@ -1,13 +1,13 @@
 export const urlArray: string[] = [
   "https://feeds.expressen.se/dinapengar",
   "http://expressen.se/rss/debatt",
-  "http://gt.se/rss/nyheter",
+  "http://expressen.se/rss/sport",
   "https://feeds.expressen.se/kvallsposten",
   "http://expressen.se/rss/halsa",
   "http://expressen.se/rss/kultur",
   "http://expressen.se/rss/ledare",
   "http://expressen.se/rss/motor",
   "http://expressen.se/rss/noje",
+  "http://gt.se/rss/nyheter",
   "http://expressen.se/rss/res",
-  "http://expressen.se/rss/sport",
 ];
