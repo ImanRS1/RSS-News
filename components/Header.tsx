e77cc06f98c705876b4e555134a7a0c354ad1116
Iman Reza Soltani <iman.soltani@bonniernews.se>
2023-06-12 15:57:43 +0200

    mobile styling added on Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,8 @@
 import { PropsWithChildren } from "react";
 import styled from "styled-components";
+import Theme from "../themes/theme";
+
+const theme = Theme();
 
 const Header = ({ children }: PropsWithChildren) => {
   return <HeaderStyle>{children}</HeaderStyle>;
@@ -8,6 +11,10 @@ const Header = ({ children }: PropsWithChildren) => {
 const HeaderStyle = styled.h3`
   font-size: 1.5rem;
   margin-top: 0;
+
+  ${theme.breakpoints.mobile} {
+    font-size: 1.3rem;
+  }
 `;
 
 export default Header;
